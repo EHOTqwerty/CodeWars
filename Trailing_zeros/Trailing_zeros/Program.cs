b9513227c6b0d00c9731090837a185bc32d33b3b
Maksym <qwerty@users.noreply.github.com>
2019-09-03 19:32:39 +0200

    Update

---
+++ Trailing_zeros/Trailing_zeros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Trailing_zeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y;
+            y = Calculating(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+
+        static int Calculating(int input)
+        {
+            int count = 0;
+            for (int i = 5; input / i >= 1; i *= 5)
+                count += input / i;
+            return count;   
+        }
+    }
+}
